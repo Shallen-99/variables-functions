8154c1ceee3990ad975dce30ebc94133bca62f52
Shallen-99 <shallenburks@gmail.com>
2025-11-07 12:04:41 -0500

    task 2 done

--- lab-1.js
+++ lab-1.js
@@ -1,9 +1,19 @@
-function formatFullName(firstName, lastName) {
-    function cap(name){
-        if(!name) return "Invalid name input"
-        return name.charAt(0).toUpperCase() + name.slice(1).toLowerCase()
-    }
-    return `${cap(lastName)}, ${cap(firstName)}`
-}
+// function formatFullName(firstName, lastName) {
+//     function cap(name){
+//         if(!name) return "Invalid name input"
+//         return name.charAt(0).toUpperCase() + name.slice(1).toLowerCase()
+//     }
+//     return `${cap(lastName)}, ${cap(firstName)}`
+// }
 
-console.log(formatFullName("shallen", "ashford"))
+// console.log(formatFullName("shallen", "ashford"))
+
+// function calculateTotalCost(price, quantity, taxRate){
+//     let totalCost = (price * quantity) * (1 + taxRate)
+//     if (isNaN(price, quantity, taxRate)) {
+//         return "Invalid Input"
+//     }
+//     return totalCost
+// }
+
+// console.log(calculateTotalCost(10, 10, 10))
